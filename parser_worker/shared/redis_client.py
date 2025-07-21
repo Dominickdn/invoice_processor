6820934e8bcd0c47e420829675fca2bd0541a4dc
Dominickdn <116680944+Dominickdn@users.noreply.github.com>
2025-07-21 15:16:49 +0200

    Added new env variables to example file

--- parser_worker/shared/redis_client.py
+++ parser_worker/shared/redis_client.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
     decode_responses=True,
 )
+
